e69129f9f4b3794327f4ebbfcef543d03a3b9218
Mohsin Ali <120159587+mohsin-ali9900@users.noreply.github.com>
2023-05-19 20:52:04 +0500

    Add files via upload

---
+++ Labs/ooplab3/task/task/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task
+{
+    internal class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("Hello WORLD");
+        }
+
+        public int matricMarks;
+        public int interMarks;
+        public float percentage;
+    }
+}
---
+++ Labs/ooplab3/task/task/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 project = new Class1();
+        }
+    }
+}
