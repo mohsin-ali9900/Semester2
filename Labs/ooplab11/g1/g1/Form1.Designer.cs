ce219561f34c835e43a7ed00f1feb70c27df976d
Mohsin Ali <120159587+mohsin-ali9900@users.noreply.github.com>
2023-08-22 19:54:46 +0500

    Add files via upload

---
+++ Labs/ooplab11/g1/g1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace g1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.BackgroundImage = global::g1.Properties.Resources.player;
+            this.player.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.player.Location = new System.Drawing.Point(459, 261);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(90, 58);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::g1.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.player);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Labs/ooplab11/g1/g1/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EZInput;
+
+namespace g1
+{
+    public partial class Form1 : Form
+    {
+        PictureBox monster = new PictureBox();
+        ProgressBar enemyHealth = new ProgressBar();
+        ProgressBar PlayerHealth = new ProgressBar();
+        public Form1()
+        {
+            InitializeComponent();
+            Image img = Properties.Resources.monster;
+            monster= createEnemy(img);
+            this.Controls.Add( monster );
+            setHeathBarCoord();
+            setPlayerHealthCoord();
+            Controls.Add(enemyHealth);
+            Controls.Add(PlayerHealth);
+        }
+
+        Random rnd = new Random();
+        private PictureBox createEnemy(Image img)
+        {
+            PictureBox enemy = new PictureBox();
+            enemy.Image = img;
+            enemy.Width = img.Width ;
+            enemy.Height = img.Height;
+            enemy.Left = rnd.Next(this.Left+20,this.Right-20);
+            enemy.Top = rnd.Next(10,20);
+            enemy.BackColor = Color.Transparent;
+            return enemy;
+        }
+        List<PictureBox> FireList = new List<PictureBox>();
+        List<PictureBox> FireListOfEnemy = new List<PictureBox>();
+        List<PictureBox> MeteoroidLsit = new List<PictureBox>();
+
+        private void setHeathBarCoord()
+        {
+            enemyHealth.Value = 100;
+            enemyHealth.Left = monster.Left;
+            enemyHealth.Top = monster.Top + 70;
+        }
+        private void setPlayerHealthCoord()
+        {
+            PlayerHealth.Value = 100;
+            PlayerHealth.Left = player.Left;
+            PlayerHealth.Top = player.Top + 70;
+        }
+        private void createBullet(Image img)
+        {
+            PictureBox bullet = new PictureBox();
+            bullet.Image = img;
+            bullet.Width = img.Width;
+            bullet.Height = img.Height;
+            bullet.Left = player.Left + (player.Width / 2) - 25; 
+            bullet.Top = player.Top;
+            bullet.BackColor = Color.Transparent;
+            this.Controls.Add(bullet);
+            this.FireList.Add(bullet );
+        }
+        Random Mrand = new Random();
+        private PictureBox createMeteoroids()
+        {
+            PictureBox meteoroid = new PictureBox();
+            Image Mimg = Properties.Resources.meteoroid;
+            meteoroid.Image = Mimg;
+            meteoroid.Width = 50;
+            meteoroid.Height = 50;
+            meteoroid.Left = Mrand.Next(this.Left + 20, this.Right - 20);
+            meteoroid.Top = this.Top;
+            return meteoroid;
+        }
+        private void addMeteoroidInList()
+        {
+            PictureBox meta = createMeteoroids();
+            MeteoroidLsit.Add(meta);
+            Controls.Add(meta);
+
+        }
+        private void moveMetaroid()
+        {
+            foreach(var m in MeteoroidLsit)
+            {
+                m.Top += 50;
+            }
+        }
+        private void createBulletOfEnemey(Image img)
+        {
+            Image eImg = Properties.Resources.enemyBul;
+            PictureBox bullett = new PictureBox();
+            bullett.Image = eImg;
+            bullett.Width = 50;
+            bullett.Height = 50;
+            bullett.Left = monster.Left + (monster.Width / 2) - 10;
+            bullett.Top = monster.Top + 50;
+            bullett.BackColor = Color.Transparent;
+            this.Controls.Add(bullett);
+            this.FireListOfEnemy.Add(bullett);
+        }
+        private void BulletCollisionWithPlayer()
+        {
+            for (int i = 0; i < FireListOfEnemy.Count; i++)
+            {
+                if (FireListOfEnemy[i].Bottom < 0)
+                {
+                    FireListOfEnemy[i].Dispose();
+
+                }
+                else if (FireListOfEnemy[i].Bounds.IntersectsWith(player.Bounds) && PlayerHealth.Value > 0)
+                {
+                    PlayerHealth.Value -= 10;
+                    FireListOfEnemy[i].Dispose();
+                }
+                if (PlayerHealth.Value <= 0)
+                {
+                    player.Dispose();
+                    PlayerHealth.Dispose();
+                }
+            }
+        }
+
+        string direction = "Left";
+        int count = 0;
+        int metaCount = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(Keyboard.IsKeyPressed(Key.LeftArrow) ) 
+            {
+                player.Left -= 20;
+                PlayerHealth.Left -= 20;
+            }
+            else if(Keyboard.IsKeyPressed (Key.RightArrow) )
+            {
+                player.Left += 20;
+                PlayerHealth.Left += 20;
+            }
+            else if(Keyboard.IsKeyPressed (Key.UpArrow))
+            {
+                player.Top -= 20;
+                PlayerHealth.Top -= 20;
+            }
+            else if (Keyboard.IsKeyPressed(Key.DownArrow))
+            {
+                player.Top += 20;
+                PlayerHealth.Top += 20;
+            }
+            if(Keyboard.IsKeyPressed(Key.Space))
+            {
+                Image bullet = Properties.Resources.fire;
+                createBullet(bullet);
+            }
+
+            moveBullet();
+            moveEnemyBullet();
+            moveEnemy();
+            count++;
+            metaCount++;
+            if(metaCount == 8)
+            {
+
+                addMeteoroidInList();
+                metaCount = 0;
+            }
+                moveMetaroid();
+            if (count == 8 && enemyHealth.Value > 0)
+            {
+                Image eBul = Properties.Resources.enemyBul;
+                createBulletOfEnemey(eBul);
+                count = 0;
+            }
+            for(int i = 0; i < FireList.Count; i++)
+            {
+                if(FireList[i].Bottom <0)
+                {
+                    FireList[i].Dispose();
+
+                }
+                else if(FireList[i].Bounds.IntersectsWith(monster.Bounds) && enemyHealth.Value > 0)
+                {
+                    enemyHealth.Value -= 10;
+                    FireList[i].Dispose();
+                }
+                if(enemyHealth.Value <=0 )
+                {
+                    monster.Dispose();
+                    enemyHealth.Dispose();
+                }
+            }
+            BulletCollisionWithPlayer();
+        }
+        private void moveEnemy()
+        {
+            if(direction == "Left")
+            {
+                monster.Left -= 20;
+                enemyHealth.Left -= 20;
+            }
+            else if(direction == "Right")
+            {
+                monster.Left += 20;
+                enemyHealth.Left += 20;
+            }
+            if(monster.Left < 10)
+            {
+                direction = "Right";
+            }
+            if(monster.Left > this.Width - 100)
+            {
+                direction = "Left";
+            }
+        }
+        private void moveBullet()
+        {
+            foreach(var b in FireList)
+            {
+                b.Top -= 50;
+            }
+        }
+        private void moveEnemyBullet()
+        {
+            foreach (var b in FireListOfEnemy)
+            {
+                b.Top += 50;
+            }
+        }
+        
+    }
+}
